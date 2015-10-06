dc58381751fadfac20bf90a3a368ce7589455003
Jarek Jarcec Cecho <jarcec@apache.org>
2015-10-06 14:04:27 -0700

    [csd] Added support to actually start Sqoop 2 server
    * This depends on bunch of patches that are under review at this point
    * Integration have several hardocded stuff that needs to be moved to configuration at some point
    * Logging is working (including propagation to CM)

--- csd/src/scripts/control.sh
+++ csd/src/scripts/control.sh
@@ -30,12 +30,69 @@ echo "CONF_DIR: $CONF_DIR"
 echo "Entire environment:"
 env
 
-# Defining variables expected in Sqoop 2 start scripts
+# If we're starting the server, the variable "COMMAND" will be empty and instead $1 will be "server"
+if [[ -z "$COMMAND" ]]; then
+  export COMMAND=$1
+fi
+
+# Defining variables expected in Sqoop 2 scripts
 export HADOOP_COMMON_HOME=$CDH_HADOOP_HOME
 export HADOOP_HDFS_HOME=$CDH_HDFS_HOME
 export HADOOP_YARN_HOME=$CDH_YARN_HOME
 export HADOOP_MAPRED_HOME=$CDH_MR2_HOME
 export SQOOP_CONF_DIR=$CONF_DIR
 
-echo "Starting Sqoop 2 from: $SQOOP2_PARCEL_DIRNAME"
-exec $SQOOP2_PARCEL_DIRNAME/bin/sqoop.sh server run
+# We also need to create small oustanding file that will instruct Sqoop to use the file configuration provider
+echo "sqoop.config.provider=org.apache.sqoop.core.PropertiesConfigurationProvider" > $SQOOP_CONF_DIR/sqoop_bootstrap.properties
+
+# We need to finish Sqoop 2 configuration file sqoop.properties as CM is not generating final configuration
+export CONF_FILE=$SQOOP_CONF_DIR/sqoop.properties
+
+# Most of those can be moved to CM as they are relevant there
+
+# TODO: Need to properly configure logging (including audit?)
+# Repository provider
+echo "org.apache.sqoop.repository.provider=org.apache.sqoop.repository.JdbcRepositoryProvider" >> $CONF_FILE
+echo "org.apache.sqoop.repository.schema.immutable=true" >> $CONF_FILE
+
+# JDBC repository provider configuration
+# TODO: This should be actually configurable in the UI
+echo "org.apache.sqoop.repository.jdbc.handler=org.apache.sqoop.repository.derby.DerbyRepositoryHandler" >> $CONF_FILE
+echo "org.apache.sqoop.repository.jdbc.transaction.isolation=READ_COMMITTED" >> $CONF_FILE
+echo "org.apache.sqoop.repository.jdbc.maximum.connections=10" >> $CONF_FILE
+echo "org.apache.sqoop.repository.jdbc.url=jdbc:derby:/var/lib/sqoop2/repository/db;create=true" >> $CONF_FILE
+echo "org.apache.sqoop.repository.jdbc.driver=org.apache.derby.jdbc.EmbeddedDriver" >> $CONF_FILE
+echo "org.apache.sqoop.repository.jdbc.user=sa" >> $CONF_FILE
+echo "org.apache.sqoop.repository.jdbc.password=" >> $CONF_FILE
+
+# System properties for embedded Derby configuration
+# TODO: DO I need this one?
+#org.apache.sqoop.repository.sysprop.derby.stream.error.file=@LOGDIR@/derbyrepo.log
+
+# Disable auto upgrade
+echo "org.apache.sqoop.connector.autoupgrade=false" >> $CONF_FILE
+echo "org.apache.sqoop.driver.autoupgrade=false" >> $CONF_FILE
+
+# Reloading configuration (useless in CM word)
+echo "org.apache.sqoop.core.configuration.provider.properties.sleep=60000" >> $CONF_FILE
+
+# Engines
+echo "org.apache.sqoop.submission.engine=org.apache.sqoop.submission.mapreduce.MapreduceSubmissionEngine" >> $CONF_FILE
+echo "org.apache.sqoop.submission.engine.mapreduce.configuration.directory=$CONF_DIR/yarn-conf/" >> $CONF_FILE
+echo "org.apache.sqoop.execution.engine=org.apache.sqoop.execution.mapreduce.MapreduceExecutionEngine" >> $CONF_FILE
+
+case $COMMAND in
+  upgrade)
+    echo "Starting Sqoop 2 upgrade tool"
+    exec $SQOOP2_PARCEL_DIRNAME/bin/sqoop.sh tool upgrade
+    ;;
+  server)
+    export JAVA_OPTS="-Dlog4j.configuration=file:$SQOOP_CONF_DIR/log4j.properties -Dlog4j.debug"
+    echo "Starting Sqoop 2 from: $SQOOP2_PARCEL_DIRNAME"
+    exec $SQOOP2_PARCEL_DIRNAME/bin/sqoop.sh server run
+    ;;
+  *)
+    echo "Unknown command: $COMMAND"
+    exit
+    ;;
+esac
