695d0776504ce482089c989b0c537d32d1afb087
Abraham Fine <abe@abrahamfine.com>
2015-11-10 14:34:35 -0800

    fix

--- deploy-parcels.sh
+++ deploy-parcels.sh
@@ -82,13 +82,11 @@ fi
 
 # Execute $1 on remote server
 function remote_exec() {
-  echo "Executing command: $1"
   sshpass -p $password ssh ${username}@${host} $1
 }
 
 # Copy $1 to $2 on remote server (e.g. upload)
 function remote_copy() {
-  echo "Executing command: scp $1 ${username}@${host}:$2"
   sshpass -p $password scp $1 ${username}@${host}:$2
 }
 
